4dd3b658938bc3b90d292b8e980948eba0839775
Kabir Hossain <newfxch@gmail.com>
2026-09-25 10:38:35 +0800

    P170: Intentionally break checksum

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -29,4 +29,5 @@ def test_generate_evidence():
     
     # Calculate SHA-256 checksum
     actual_hash = hashlib.sha256(evidence_bytes).hexdigest()
-    checksum_file.write_text(actual_hash)
+    #checksum_file.write_text(actual_hash)
+    checksum_file.write_text("fakehash123")
